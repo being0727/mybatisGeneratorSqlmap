bcc832c8dd2e91656c6b073289cb42c2cda9487c
xuzhansheng <xuzhansheng@bluemoon.com.cn>
2019-04-01 09:23:25 +0800

    添加 mapper生成器

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+import javax.xml.crypto.Data;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+
+public class Test {
+	
+	
+	private final static String ENCRYPT_KEY = "cap_user";
+	
+
+	
+	public static void main(String[] args) throws Exception {
+		SimpleDateFormat simpleDateFormat ;
+		Data data;
+	}
+
+}
